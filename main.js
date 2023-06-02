1856f41d309a4655dbf08e74fa644dcc3551bd2d
Rawle Juglal <rawledjuglal@gmail.com>
2023-06-02 15:35:24 -0500

    Update main.js
    updated main to match

--- main.js
+++ main.js
@@ -3,22 +3,19 @@ import './style.css'
 const chatbotConversation = document.getElementById('chatbot-conversation')
  
 let conversationStr = ''
-
  
 document.addEventListener('submit', (e) => {
     e.preventDefault()
-    const userInput = document.getElementById('user-input')
-    conversationStr += `${userInput.value} ->`
-
+    const userInput = document.getElementById('user-input') 
+    conversationStr += ` ${userInput.value} ->`
     fetchReply()
-    
     const newSpeechBubble = document.createElement('div')
     newSpeechBubble.classList.add('speech', 'speech-human')
     chatbotConversation.appendChild(newSpeechBubble)
     newSpeechBubble.textContent = userInput.value
     userInput.value = ''
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-})
+}) 
 
 async function fetchReply(){
     const url = 'https://serene-queijadas-27b8a2.netlify.app/.netlify/functions/fetchAI'
@@ -31,12 +28,11 @@ async function fetchReply(){
         body: conversationStr
     })
     const data = await response.json()
-    
-    conversationStr += ` ${data.reply.choices[0].text} ->`
+    conversationStr+=` ${data.reply.choices[0].text} ->`
     renderTypewriterText(data.reply.choices[0].text)
-    console.log(data)    
+    console.log(data)
 }
-
+ 
 function renderTypewriterText(text) {
     const newSpeechBubble = document.createElement('div')
     newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
@@ -52,27 +48,3 @@ function renderTypewriterText(text) {
         chatbotConversation.scrollTop = chatbotConversation.scrollHeight
     }, 50)
 }
-
-// document.getElementById('clear-btn').addEventListener('click', () => {
-//     remove(conversationInDB)
-//     chatbotConversation.innerHTML = '<div class="speech speech-ai">How can I help you?</div>'
-// })
-
-function renderConversationFromDb(){
-    get(conversationInDB).then(async (snapshot)=>{
-        if(snapshot.exists()) {
-            Object.values(snapshot.val()).forEach(dbObj => {
-                const newSpeechBubble = document.createElement('div')
-                newSpeechBubble.classList.add(
-                    'speech',
-                    `speech-${dbObj.role === 'user' ? 'human' : 'ai'}`
-                    )
-                chatbotConversation.appendChild(newSpeechBubble)
-                newSpeechBubble.textContent = dbObj.content
-            })
-            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-        }
-    })
-}
-
-renderConversationFromDb()
